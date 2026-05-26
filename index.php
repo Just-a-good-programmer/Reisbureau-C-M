4d910db7e244168a27b654cc3571ed262dc78d77
CarmenV446 <1220871@student.roc-nijmegen.nl>
2026-05-26 11:30:23 +0200

    search box made
    made the search box, with the necessary inputs, i also made the style with css so html + css for searchbox is practically done

--- index.php
+++ index.php
@@ -7,25 +7,42 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <header>
-        
-        
-    </header>
-    <main>
-        <form action="" method="get">
-            <select>
-                <option value="Vliegreis">Vliegreis</option>
-                <option value="Treinreis">Treinreis</option>
-                <option value="Autoreis">Autoreis</option>
-            </select>
+<header>
 
-            <input placeholder="Hoeveel personen?" type="number" name="personen">
-            <input type="submit" value="Zoek">
+
+</header>
+<main>
+    <div class="box">
+        <div class="box-title">Vakanties</div>
+        <form class="searchbox" action="" method="get">
+            <div class="column">
+                <label>Vakantietype</label>
+                <select class="input-box">
+                    <option selected>geen voorkeur</option>
+                    <option value="Vliegreis">Vliegreis</option>
+                    <option value="Treinreis">Treinreis</option>
+                    <option value="Autoreis">Autoreis</option>
+                </select>
+            </div>
+            <div class="column">
+                <label>Bestemming</label>
+                <input class="input-box" type="text" placeholder="bestemming">
+            </div>
+            <div class="column">
+                <label>Vertrekdatum</label>
+                <input class="input-box" type="date" placeholder="datum">
+            </div>
+            <div class="column">
+                <label>Hoeveel personen</label>
+                <input class="input-box" placeholder="Hoeveel personen?" type="number" name="personen">
+            </div>
+            <input class="searchbutton" type="submit" value="Zoeken">
         </form>
-    </main>
+    </div>
+</main>
 
-    <footer>
+<footer>
 
-    </footer>
+</footer>
 </body>
 </html>
