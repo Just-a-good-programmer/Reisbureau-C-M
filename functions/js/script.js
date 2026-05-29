8156bc07e59a04dfe521aed76d4273f39f653770
smth1808 <MB.Straub@gmail.com>
2026-05-29 11:42:13 +0200

    Added filter checking and unchecking
    -Added basic filter functionality (without the php)
    -Added javascript to (un)check the airport boxes all at once with the all airports checkbox

--- functions/js/script.js
+++ functions/js/script.js
@@ -12,3 +12,33 @@ document.getElementById("alle-luchthavens").addEventListener("click", function()
     }
 });
 
+var alleSterren = document.getElementById("alle-sterren");
+alleSterren.addEventListener("click", function() {
+    if (this.checked) {
+        starFilterItems = document.getElementsByClassName("star-filter-item");
+        for (var i = 0; i < starFilterItems.length; i++) {
+            if(this.checked) {
+                starFilterItemsChecked.push(starFilterItems[i].value);
+            } else {
+                starFilterItems[i].checked = false;
+            }
+        }
+
+var reviewFilterItems = document.getElementsByClassName("review-filter-item");
+const reviewFilterItemsChecked = [];
+for (var i = 0; i < reviewFilterItems.length; i++) {
+    reviewFilterItems[i].addEventListener("click", function() {
+        if (this.checked) {
+            // Code to filter reviews based on the selected rating
+              reviewFilterItemsChecked.push(this.value);
+
+        } else {
+            reviewFilterItemsChecked.splice(reviewFilterItemsChecked.indexOf(this.value), 1);
+            i++; // Increment the index to check the next filter item
+            
+        }
+        document.getElementById("review-checked").innerHTML = reviewFilterItemsChecked;
+    });
+    
+}
+    
