86a83764b89f4960eb52db9a622a0c1ecef6b3e3
bogosprinted <1218067@student.roc-nijmegen.nl>
2026-06-01 16:14:52 +0200

    contact pagina
    ik heb de input bij contact gemaakt

---
+++ contact.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<body lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<header>
+    <div class="hamburgermenu">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </div>
+
+    <h1>Contact</h1>
+</header>
+
+<div class="contact">
+    <h1>Contact</h1>
+    E-mailadres
+    <input class="mail" placeholder="e-mail">
+    Onderwerp
+    <input class="mail" placeholder="onderwerp" role="option">
+    Beschrijving
+    <input class="beschrijving" placeholder="beschrijving">
+    Telefoonnummer(optioneel)
+    <input class="mail" placeholder="telefoonnummer">
+    <button class="send">Verzenden</button>
+</div>
+
+</body>
+
+</html>
